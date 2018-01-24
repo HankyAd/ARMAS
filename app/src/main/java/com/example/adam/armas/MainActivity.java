44d09b8235bb109e1f7160a910b6a2332fdb7f4f
Karachristou <panayiotis95@rocketmail.com>
2018-01-24 10:28:44 +0000

    Added Camera Permissions. Almost got camera working.

--- app/src/main/java/com/example/adam/armas/MainActivity.java
+++ app/src/main/java/com/example/adam/armas/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.adam.armas;
-
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.ViewGroup;
+import java.util.HashMap;
 import cn.easyar.*;
 
 
 public class MainActivity extends AppCompatActivity {
+    private GLView glView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         String key = "Dvwi44jdfu7RFcq2jN6vFBNUKTHiStKtfoevOz3CzTKGtz4J1tHCu9MtyYorV0sJGJFQs747c7Uu07S39cYooN4sNqBeE7gi8DRg8oZN25VdKdVbK3csqyWPuBNoqmy9FimsvyOU81Bd0LB9XX8Gga13OOp33x1XFtod00m7Wh3xsn8f9VCfsIIucdki8OpnQeuswZNo";
@@ -22,36 +28,134 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                CameraDevice cd = new CameraDevice();
-                CameraFrameStreamer cfs = new CameraFrameStreamer();
-                cfs.attachCamera(cd);
+
+            }
+        });
+
+
+///copied from HelloAr for camera permissions
+
+
+        glView = new GLView(this);
+
+        requestCameraPermission(new PermissionCallback() {
+            @Override
+            public void onSuccess() {
+       //       ((ViewGroup) findViewById(R.id.preview)).addView(glView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            }
+
+            @Override
+            public void onFailure() {
             }
         });
     }
 
+    private interface PermissionCallback {
+        void onSuccess();
+
+        void onFailure();
+    }
+
+    private HashMap<Integer, PermissionCallback> permissionCallbacks = new HashMap<Integer, PermissionCallback>();
+    private int permissionRequestCodeSerial = 0;
+
+    @TargetApi(23)
+    private void requestCameraPermission(PermissionCallback callback) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                int requestCode = permissionRequestCodeSerial;
+                permissionRequestCodeSerial += 1;
+                permissionCallbacks.put(requestCode, callback);
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, requestCode);
+            } else {
+                callback.onSuccess();
+            }
+        } else {
+            callback.onSuccess();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (permissionCallbacks.containsKey(requestCode)) {
+            PermissionCallback callback = permissionCallbacks.get(requestCode);
+            permissionCallbacks.remove(requestCode);
+            boolean executed = false;
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    executed = true;
+                    callback.onFailure();
+                }
+            }
+            if (!executed) {
+                callback.onSuccess();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (glView != null) {
+            glView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (glView != null) {
+            glView.onPause();
+        }
+        super.onPause();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+    ////untill here
 }
+
+
+
+
+
+      //  @Override
+    //public boolean onCreateOptionsMenu(Menu menu) {
+      //       Inflate the menu; this adds items to the action bar if it is present.
+        //    getMenuInflater().inflate(R.menu.menu_main, menu);
+          //  return true;
+    //}
+    //@Override
+    //public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+    //    int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+      //  if (id == R.id.action_settings) {
+        //    return true;
+      //  }
+
+       // return super.onOptionsItemSelected(item);
+   // }
+// }
