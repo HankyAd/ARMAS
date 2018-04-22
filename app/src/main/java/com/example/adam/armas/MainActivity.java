96403839d177851619075e96f19680d5f4eb7a37
Adam Hankin <adam.hnkn@gmail.com>
2018-04-22 22:06:13 +0100

    help

--- app/src/main/java/com/example/adam/armas/MainActivity.java
+++ app/src/main/java/com/example/adam/armas/MainActivity.java
@@ -64,10 +64,25 @@ public class MainActivity extends AppCompatActivity{
     public interface MessageAlerter {
         void invoke(String s);
     }
-
+    private static final int REQUEST_WRITE_PERMISSION = 786;
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_WRITE_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            openFilePicker();
+        }
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
+        } else {
+            openFilePicker();
+        }
+    }
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestPermission();
         DAO dao = new DAO(this);
         dataAccessObject = dao;
         System.out.println(dataAccessObject.getAsbIDByImageName("demo1"));
@@ -142,6 +157,7 @@ public class MainActivity extends AppCompatActivity{
             public void onFailure() {
             }
         });
+
     }
 
 
