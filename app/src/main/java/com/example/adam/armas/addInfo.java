d68f199926708a0b673f6fcaf6800942233c3568
Karachristou <panayiotis95@rocketmail.com>
2018-04-23 23:52:47 +0100

    IMAGE SELECTION ADDED

--- app/src/main/java/com/example/adam/armas/addInfo.java
+++ app/src/main/java/com/example/adam/armas/addInfo.java
@@ -2,13 +2,18 @@ package com.example.adam.armas;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,7 +24,8 @@ public class addInfo extends AppCompatActivity {
     private DAO dao;
     private String imageName = "";
     private MainActivity mainActivity;
-
+    private static int RESULT_LOAD_IMG = 1;
+    String imgDecodableString;
     public addInfo() {
         super();
     }
@@ -34,6 +40,16 @@ public class addInfo extends AppCompatActivity {
 
 
         FloatingActionButton test = (FloatingActionButton) findViewById(R.id.floatingActionButton3);
+        final Button button = (Button) findViewById(R.id.button10);
+        final ImageView imgView = (ImageView) findViewById(R.id.imageView5);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                loadImagefromGallery(imgView);
+            }
+
+        });
+
+
         test.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 finish();
@@ -43,6 +59,54 @@ public class addInfo extends AppCompatActivity {
 
 
 
+    }
+
+
+
+
+    public void loadImagefromGallery(View view) {
+        // Create intent to Open Image applications like Gallery, Google Photos
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Start the Intent
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                ImageView imgView = (ImageView) findViewById(R.id.imageView5);
+                // Set the Image in ImageView after decoding the String
+                imgView.setImageBitmap(BitmapFactory
+                        .decodeFile(imgDecodableString));
+
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
 
     }
+
 }
